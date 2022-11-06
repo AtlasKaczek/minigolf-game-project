3129668b95be316a533f063acfb148cf23ed8bfb
AtlasKaczek <stan.kryj@gmail.com>
2022-11-06 23:41:03 +0100

    progress

---
+++ Makefile
@@ -0,0 +1,12 @@
+# Builds an executable of the game
+build:
+	g++ -o minigolf main.cpp -lSDL2
+
+
+# Runs the game
+run:
+	./minigolf
+
+
+# Initialize
+
--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <SDL2/SDL.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 int main()
 {
+	SDL_Window* window = NULL;
+	SDL_Surface* screenSurface = NULL;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not init! SDL err: %s\n", SDL_GetError());
+	} else {
+		window = SDL_CreateWindow("Minigolf GAME", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (window == NULL)
+		{
+			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+		} else {
+            screenSurface = SDL_GetWindowSurface( window );
+
+            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+            
+            SDL_UpdateWindowSurface( window );
+
+            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+		}
+		
+	}
 	
+
+	SDL_DestroyWindow( window );
+	SDL_Quit();
+
 	return 0;
 }
